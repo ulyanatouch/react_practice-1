10437268cac9c84b93c7ee1ca32ccbb0fc0a519f
Uliana B. <ulyana9963@gmail.com>
2023-12-11 12:58:30 +0100

    add changes to  my first react project

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import React from "react";
 import { Post } from "./components/Post/Post";
 import { Todo } from "./components/Todo/Todo";
 
+// экспорт дефолтный - без фигурных скобок
+// экспорт обычный - с фигурными скобками
+
+// могу прописать логику
+// return всегда возвращает один тег
+// один родительский элемент обарчивает все остальные
+// JSX - смесь html + js - функция которая возвращает html
+
 function App() {
   const postData = {
-    title: 'Post number 1',
-    description: 'Hello World',
+    title: "Mummelsee",
+    description: "Ein See voller Mythen und Sagen",
     marked: true,
-  }
+  };
 
   const postData1 = {
-    title: 'Post number 1',
-    description: 'Hello World',
+    title: "Mummelsee",
+    description:
+      "So viele Sagen und Mythen wie der Mummelsee hat kein anderer See im Schwarzwald zu bieten.",
     marked: true,
-  }
+  };
   return (
     <div className="App">
-      <Post taskTitle={postData.title} taskDescription= {postData.taskDescription} />
-      <Post {...postData1}/>
+      <Post
+        taskTitle={postData.title}
+        taskDescription={postData.taskDescription}
+      />
+      <Post {...postData1} />
       <Todo />
     </div>
   );
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,27 +3,28 @@ import React from "react";
 import mummelsee from "../../images/mummelsee.jpeg";
 
 import classes from './post.module.css'
+console.log(classes) // объект
 
-export const Post = ({ taskTitle, taskDescription }) => {
+export const Post = ({ taskTitle, taskDescription, marked }) => {
   return (
-    <div style={{color: 'blue'}} className={classes.postContainer}>
-      <h1 className={classes.postTitle}>{taskTitle}</h1>
-      <p className={classes.postDescription}>{taskDescription}</p>
-      <img className={classes.postImage} src={mummelsee} alt="mummelsee" />
+    <div className={marked ? classes.marked : classes.postContainer}>
+      <h1>{taskTitle}</h1>
+      <p>{taskDescription}</p>
+      <img src={mummelsee} alt="mummelsee" />
     </div>
   );
 };
 
-// const Post1 = (props) => {
-//   const {title, description} = props
-//   return (
-//     <div>
-//       <h1>{title}</h1>
-//       <p>{description}</p>
-//       <img src={mummelsee} alt="mummelsee" />
-//     </div>
-//   );
-// };
+export const Post1 = (props) => {
+  const {title, description} = props
+  return (
+    <div style={{ width: '400px', backgroundColor: 'green' }}>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <img src={mummelsee} alt="mummelsee" />
+    </div>
+  );
+};
 
 // const Post2 = (props) => {
 //   const {title, description} = props
@@ -35,3 +36,6 @@ export const Post = ({ taskTitle, taskDescription }) => {
 //     </div>
 //   );
 // };
+
+// default может быть только один в файле
+// могу переименовать при импорте
